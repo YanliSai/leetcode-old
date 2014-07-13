227a3b35fc258d6e111ddb270b9cc3c3ff4139f4
Yanli Sai <yanlisai@gmail.com>
2014-07-13 15:09:29 -0400

    Add solution

--- LeetCode/src/com/leetcode/MaximumDepthOfBinaryTree/Solution2.java
+++ LeetCode/src/com/leetcode/MaximumDepthOfBinaryTree/Solution2.java
@@ -1,37 +1,35 @@
 package com.leetcode.MaximumDepthOfBinaryTree;
 
-import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.Map;
 
 import com.leetcode.core.TreeNode;
 
-// AC - Non-recursion: BFS
+//AC - Non-recursion: BFS
 public class Solution2 {
-   public int maxDepth(TreeNode root) {
-       if (root == null) {
-           return 0;
-       }
-       
-       LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
-       Map<TreeNode, Integer> map = new HashMap<TreeNode, Integer>(); // Store level of each node
-       
-       queue.add(root);
-       map.put(root, 1);
-       TreeNode node = null;
-       while (!queue.isEmpty()) {
-           node = queue.removeFirst();
-           int level = map.get(node); // The level of the node
-           if (node.left != null) {
-               queue.add(node.left);
-               map.put(node.left, level + 1);
-           }
-           if (node.right != null) {
-               queue.add(node.right);
-               map.put(node.right, level + 1);
-           }
-       }
-       
-       return map.get(node).intValue(); // The maximum length is the level
-   }
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        
+        LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        int level = 0;
+
+        while (!queue.isEmpty()) {
+            level++;
+           
+            int size = queue.size(); // !!! The queue size is changing
+            for (int i = 0; i < size; i++) { // each level
+                TreeNode node = queue.removeFirst();
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+        }
+        
+        return level; // The maximum length is the level
+    }
 }
---
+++ LeetCode/src/com/leetcode/MinimumDepthOfBinaryTree/Solution.java
@@ -0,0 +1,25 @@
+package com.leetcode.MinimumDepthOfBinaryTree;
+
+import com.leetcode.core.TreeNode;
+
+// AC - Recursion
+public class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        
+        if (root.left == null) {
+            return minDepth(root.right) + 1;
+        }
+        
+        if (root.right == null) {
+            return minDepth(root.left) + 1;
+        }
+        
+        int left = minDepth(root.left) + 1;
+        int right = minDepth(root.right) + 1;
+        
+        return Math.min(left, right);
+    }
+}
