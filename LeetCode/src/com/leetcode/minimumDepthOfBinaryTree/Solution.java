7fefccdaf1cb6fe8d10c424307422f77e212aef9
Yanli Sai <yanlisai@gmail.com>
2014-07-14 23:12:17 -0400

    Minimum Depth of Binary Tree

--- LeetCode/src/com/leetcode/minimumDepthOfBinaryTree/Solution.java
+++ LeetCode/src/com/leetcode/minimumDepthOfBinaryTree/Solution.java
@@ -1,4 +1,4 @@
-package com.leetcode.MinimumDepthOfBinaryTree;
+package com.leetcode.minimumDepthOfBinaryTree;
 
 import com.leetcode.core.TreeNode;
 
--- LeetCode/src/com/leetcode/minimumDepthOfBinaryTree/Solution2.java
+++ LeetCode/src/com/leetcode/minimumDepthOfBinaryTree/Solution2.java
@@ -1,4 +1,4 @@
-package com.leetcode.MinimumDepthOfBinaryTree;
+package com.leetcode.minimumDepthOfBinaryTree;
 
 import java.util.LinkedList;
 
